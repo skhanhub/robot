23a7887e0fd812f9703fd6a8c0e74b3158bf6bd0
hasankhs <shahriar.hasankhan@anz.com>
2021-06-27 22:01:56 +1000

    remove redundent types

--- src/services/runCommands.ts
+++ src/services/runCommands.ts
@@ -1,18 +1,6 @@
 // This file contains the runCommands class
 import { promises } from 'fs';
-import ToyRobot, { ActionMap } from './toyRobot';
-
-
-type  Position = {
-  X: number,
-  Y: number,
-  F: string,
-}
-
-type ReturnPosition = {
-  valid: boolean,
-  position: Position
-}
+import ToyRobot, { ActionMap, ReturnPosition } from './toyRobot';
 
 const radix = 10;
 
