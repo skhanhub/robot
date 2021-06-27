51226552c7ae83858ba6ff43ab8847b7718599ed
hasankhs <shahriar.hasankhan@anz.com>
2021-06-27 21:42:41 +1000

    add class with constructor

--- src/services/runCommands.ts
+++ src/services/runCommands.ts
@@ -13,3 +13,19 @@ type ReturnPosition = {
   valid: boolean,
   position: Position
 }
+
+export default class runCommands {
+  toyRobot: ToyRobot;
+  commands: Array<string>;
+  actionMap: ActionMap;
+  print: boolean;
+
+  constructor(toyRobot = new ToyRobot(), commands = [], print = false){
+
+    this.toyRobot = toyRobot;
+    this.commands = commands;
+    this.print = print;
+    this.actionMap = toyRobot.GetActionMap();
+  }
+
+} // End of class
