47a91283b118848bf983f495c233b15ff34e1d3b
hasankhs <shahriar.hasankhan@anz.com>
2021-06-27 21:04:44 +1000

    app config

---
+++ config/index.ts
@@ -0,0 +1,11 @@
+ const config = {
+  development: {
+    appName: 'Toy Robot Simulator [Development]'
+  },
+  production: {
+    appName: 'Toy Robot Simulator'
+  },
+}
+
+
+export default config;
