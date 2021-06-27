feae1466ed88dfaab5efea0ecb2df7b4a1319c08
hasankhs <shahriar.hasankhan@anz.com>
2021-06-27 21:43:40 +1000

    add setCommands and getCommandsFromFile method

--- src/services/runCommands.ts
+++ src/services/runCommands.ts
@@ -27,5 +27,25 @@ export default class runCommands {
     this.print = print;
     this.actionMap = toyRobot.GetActionMap();
   }
+  //Method for setting an array of commands
+  SetCommands = (commands: Array<string>) => this.commands = commands;
+  /*
+  Method for reading the commands for the robot from a file
+  The Method takes the path to a file as the argument
+  The Method returns the commands as an array
+  */
+  GetCommandsFromFile = async (path: string): Promise<Array<string>> => {
+
+    console.log(`Reading commands from ${path}`)
+    const data: string = await promises.readFile(path, 'utf8');
+
+    if (!data){
+      this.commands = [];
+      return [];
+    }
+    // One command per line. Filter out empty lines
+    this.commands = data.split('\n').filter( command => command !== '');
+    return this.commands;
+  }
 
 } // End of class
