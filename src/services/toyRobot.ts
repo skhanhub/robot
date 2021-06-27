b4034a9314c17e9587ccc0c49e175c8e9c695ea4
hasankhs <shahriar.hasankhan@anz.com>
2021-06-27 21:37:43 +1000

    add ToyRobot class with constructor

--- src/services/toyRobot.ts
+++ src/services/toyRobot.ts
@@ -33,3 +33,29 @@ export const defaultPosition = {
   Y: 0,
   F: 'North',
 }
+
+// Class for simulating a toy robot
+export default class ToyRobot {
+  directionLookUp: LookUp;
+  position: Position;
+  valid: boolean;
+  dimention: number;
+
+  constructor(
+    directionLookUp = {
+      north: {X: 0, Y: 1, Left: 'West', Right: 'East'},
+      south: {X: 0, Y: -1, Left: 'East', Right: 'West'},
+      east: {X: 1, Y: 0, Left: 'North', Right: 'South'},
+      west: {X: -1, Y: 0, Left: 'South', Right: 'North'},
+    },
+    position = defaultPosition,
+    valid = false,
+    dimention = 5
+  ){
+    this.directionLookUp = directionLookUp;
+    this.position = position;
+    this.valid = valid;
+    this.dimention = dimention;
+  }
+
+}// End Class
